ee991f27afdc72defddfefdca6d3421351580596
Charlie <45036245+charliemday@users.noreply.github.com>
2026-07-04 11:59:55 +0100

    Initial commit

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'unlock-london-v2';
+const SHELL = [
+  './',
+  './index.html',
+  './style.css',
+  './landmarks.json',
+  './manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).catch(() => cached);
+    })
+  );
+});
